f6e45ac62161b166738b0d74152c09bfa70fa3fd
JunioBatista <juniorjob21@gmail.com>
2022-04-29 22:07:32 -0300

    configurações inciais e rotas funcionando

---
+++ src/routes/cordinator.js
@@ -0,0 +1,21 @@
+export const goToHome = (navigate) => {
+    navigate("./")
+}
+
+export const goToCheckoutPage = (navigate) => {
+    navigate("./checkout")
+}
+
+export const goToProductPage = (navigate) => {
+    navigate("./product")
+}
+
+export const goToProductListPage = (navigate) => {
+    navigate("./list")
+}
+
+export const goToErrorPage = (navigate) => {
+    navigate("./*")
+}
+
+
